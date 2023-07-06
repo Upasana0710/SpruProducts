f52f85f2d10d2b429eb17920ba088bebda1b4280
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-07-06 23:11:32 +0530

    added theme colors

--- src/utils/Theme.js
+++ src/utils/Theme.js
@@ -1,7 +1,7 @@
 export const lightTheme = {
     bg: "#FFFFFF",
     bgLight: "#f0f0f0",
-    primary: "#A0C49D",
+    primary: "#216b5d",
     secondary: "#C4D7B2",
     text_primary: "#111111",
     text_secondary: "#48494a",
