2e374096deea2d25a7bfd09ba51df30478720e1c
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-07-06 23:11:24 +0530

    added a home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import styled from 'styled-components';
+import HeroSlider from '../components/HeroSlider';
+import Welcome from '../components/Welcome';
 
+const HomeContainer = styled.div`
+display: flex;
+flex-direction: column;
+`;
 const Home = () => {
   return (
-    <div>Home</div>
+    <HomeContainer>
+      <HeroSlider />
+      <Welcome />
+    </HomeContainer>
   )
 }
 
