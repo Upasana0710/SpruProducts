d289b1e5861350ec23c4cf438d6621bd92eda272
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-07-07 18:16:34 +0530

    changes color

--- src/utils/Theme.js
+++ src/utils/Theme.js
@@ -2,7 +2,7 @@ export const lightTheme = {
     bg: "#FFFFFF",
     bgLight: "#f0f0f0",
     primary: "#216b5d",
-    secondary: "#C4D7B2",
+    secondary: "#004f17",
     text_primary: "#111111",
     text_secondary: "#48494a",
     card: "#FFFFFF",
