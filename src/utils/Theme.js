fff8152912ee1e23458b736be52e97ade03bd27c
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-07-08 15:29:44 +0530

    changed secondary color

--- src/utils/Theme.js
+++ src/utils/Theme.js
@@ -2,7 +2,7 @@ export const lightTheme = {
     bg: "#FFFFFF",
     bgLight: "#f0f0f0",
     primary: "#216b5d",
-    secondary: "#004f17",
+    secondary: "#43995a",
     text_primary: "#111111",
     text_secondary: "#48494a",
     card: "#FFFFFF",
