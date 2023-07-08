ae1a7e95820edb2e6e85eb263c2eca8f3a863606
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-07-08 20:21:52 +0530

    fixed menu and navbar issue

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import { useState, useEffect } from 'react';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import styled, { ThemeProvider } from 'styled-components';
 import Home from '../src/pages/Home';
-import TopBar from '../src/components/TopBar';
 import Navbar from '../src/components/Navbar';
-import All from '../src/components/All';
 import { lightTheme } from "./utils/Theme";
 import Footer from './components/Footer';
+import Menu from '../src/components/Menu';
+import TopBar from '../src/components/TopBar';
 
 const Spru = styled.div`
   display: flex;
@@ -16,30 +17,76 @@ const Spru = styled.div`
   overflow-x: hidden;
   overflow-y: hidden;
 `;
+
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   flex: 3;
+  position: relative;
 `;
+
+const FixContainer = styled.div`
+  width: 100%;
+  position: fixed;
+  z-index: 900;
+`;
+
+const TopBarContainer = styled.div`
+  height: 50px;
+  background: ${({ theme }) => theme.bgLight};
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 800;
+  transition: transform 0.3s ease-in-out;
+  transform: translateY(${({ hidden }) => (hidden ? '-100%' : '0')});
+`;
+
+const Pages = styled.div`
+  margin-top: 150px;
+`;
+
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [hideTopBar, setHideTopBar] = useState(false);
+
+  const handleScroll = () => {
+    const scrollTop = window.pageYOffset;
+    if (scrollTop > 50) {
+      setHideTopBar(true);
+    } else {
+      setHideTopBar(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <ThemeProvider theme={lightTheme}>
       <div className="App">
         <BrowserRouter>
-            <Spru>
-              <Container>
-                <TopBar />
-                <All />
+          <Spru>
+            {menuOpen && <Menu menuOpen={menuOpen} />}
+            <Container>
+              <FixContainer>
+                <TopBarContainer hidden={hideTopBar}>
+                  <TopBar />
+                  <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+                </TopBarContainer>
+                <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+              </FixContainer>
+              <Pages>
                 <Routes>
-                  <Route
-                    path="/"
-                    exact
-                    element= <Home/>
-                  />
+                  <Route path="/" exact element={<Home />} />
                 </Routes>
-                <Footer />
-              </Container>
-            </Spru>
+              </Pages>
+              <Footer />
+            </Container>
+          </Spru>
         </BrowserRouter>
       </div>
     </ThemeProvider>
